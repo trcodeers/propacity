fc4d289857bcc762982ffa6fba1a4bae40200708
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-12-16 11:46:23 +0530

    add note card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NoteCard from './components/noteCard';
+
+
 
 function App() {
 
+  const onClickAddNote = () =>{
 
+  }
 
   return (
     <div className="App">
-      <button className='bg-red-100'>Add Notes</button>
+      <button onClick={onClickAddNote} className='bg-red-100'>Add Notes</button>
+      <NoteCard
+        title="THis is title"
+        description="THis is a description"
+      />
     </div>
   );
 }
---
+++ src/components/noteCard.tsx
@@ -0,0 +1,36 @@
+import { MdDelete, MdEdit } from "react-icons/md";
+
+const NoteCard = (props: any) =>{
+
+    const { title, description } = props
+    return(
+
+        <div className="w-[200px] h-36 rounded overflow-hidden shadow-lg bg-red-50 relative">
+        <div className="px-2 py-2 text-center">
+          <div className="font-bold text-l mb-2 text-center overflow-hidden line-clamp-1">{title}</div>
+  
+          <p className="text-gray-700 text-base overflow-hidden line-clamp-3">{description}</p>
+        </div>
+  
+        <div className="absolute bottom-0 right-0 mb-1 mr-2">
+          <div className="flex flex-row justify-center gap-4">
+            <div
+            //   onClick={() => openModal(item, index, status)}
+              className="inline-block text-blue-700 bg-green-100 rounded-full px-2 py-"
+            >
+              <MdEdit />
+            </div>
+            <div
+            //   onClick={() => onDelete(item, index, status)}
+              className="inline-block text-red-500 bg-green-100 rounded-full px-2 py-"
+            >
+              <MdDelete />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+}
+
+export default NoteCard;
