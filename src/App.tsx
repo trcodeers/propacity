1527b045c6644d886d0f61faa8d34489d7c8f3e8
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-12-16 14:33:54 +0530

    add color to add note button

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import NoteCard from "./components/noteCard";
 import NoteModal from "./components/notesFormModal";
-import { createNewNote, getAllNotes, deleteNote } from "./services/notes";
-import { useSnackbar } from 'notistack';
+import {
+  createNewNote,
+  getAllNotes,
+  deleteNote,
+  editNote,
+} from "./services/notes";
+import { useSnackbar } from "notistack";
 
 function App() {
-
   const { enqueueSnackbar } = useSnackbar();
 
   const [modalOpen, setModalOpen] = useState(false);
   const [notes, setNotes] = useState([]);
 
-  const [noteToEdit, setNoteToEdit] = useState(null)
+  const [noteToEdit, setNoteToEdit] = useState(null);
 
   useEffect(() => {
     setNotes(getAllNotes());
@@ -28,44 +32,53 @@ function App() {
 
   const onClickEdit = (data: any) => {
     console.log("click edit", data);
-    setNoteToEdit(data)
-    setModalOpen(true)
-
+    setNoteToEdit(data);
+    setModalOpen(true);
   };
-
 
   const updateNote = (data: any) => {
     if (data.id) {
       console.log("update existing note", data);
+      editNote(data);
     } else {
       console.log("Create new note", data);
       const res = createNewNote(data);
       if (res) {
         setNotes(getAllNotes());
-        enqueueSnackbar('Note created!', { autoHideDuration: 1000, variant: 'success', anchorOrigin: {vertical: 'top', horizontal: 'right'}  })
+        enqueueSnackbar("Note created!", {
+          autoHideDuration: 1000,
+          variant: "success",
+          anchorOrigin: { vertical: "top", horizontal: "right" },
+        });
       } else {
         console.log("Failed to create");
       }
     }
-    setModalOpen(false)
+    setModalOpen(false);
   };
 
-  const onClickDelete = (id: any) =>{
-    const res = deleteNote(id)
-    if(res) {
+  const onClickDelete = (id: any) => {
+    const res = deleteNote(id);
+    if (res) {
       setNotes(getAllNotes());
-      console.log('Deleted successfuly!')
-      enqueueSnackbar('Deleted successfuly!', { autoHideDuration: 1000, variant: 'error', anchorOrigin: {vertical: 'top', horizontal: 'right'}  })
+      console.log("Deleted successfuly!");
+      enqueueSnackbar("Deleted successfuly!", {
+        autoHideDuration: 1000,
+        variant: "error",
+        anchorOrigin: { vertical: "top", horizontal: "right" },
+      });
+    } else {
+      console.log("Failed to delete");
     }
-    else{
-      console.log('Failed to delete')
-    }
-  }
+  };
 
   return (
     <div className="App">
-      <button onClick={onClickAddNote} className="bg-red-100">
-        Add Notes
+      <button
+        onClick={onClickAddNote}
+        className="bg-red-500 text-white font-semibold py-2 px-3 rounded-sm mt-5 "
+      >
+        Add Note
       </button>
 
       <NoteModal
