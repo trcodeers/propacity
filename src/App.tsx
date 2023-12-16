b08a39d2246304a8013d2927b39fd391e9625886
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-12-16 12:09:49 +0530

    add modal to add notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NoteCard from './components/noteCard';
+import NoteModal from './components/notesModal';
 
 
 
 function App() {
 
-  const onClickAddNote = () =>{
+  const [modalOpen, setModalOpen] = useState(false)
 
+  const onClickAddNote = () =>{
+    setModalOpen(true)
   }
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div className="App">
       <button onClick={onClickAddNote} className='bg-red-100'>Add Notes</button>
+
+      <NoteModal
+        isOpen={modalOpen}
+        formData={null}
+        updateTask={null}
+        onClose={closeModal}
+      />
+
       <NoteCard
         title="THis is title"
         description="THis is a description"
---
+++ src/components/notesModal.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { IoMdClose } from "react-icons/io";
+// import { TaskData } from "../types/items";
+
+type Props = {
+  isOpen: boolean 
+  onClose: any
+  formData: any
+  updateTask: any
+}
+const NoteModal = ({ isOpen, onClose, formData, updateTask, } : Props) => {
+  
+    const { register, handleSubmit, reset } = useForm({
+        defaultValues:{
+            name: formData?.name || '' ,
+            description: formData?.description || '',
+            date: formData?.date || ''
+        }
+    });
+
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    // updateTask(data);
+    reset()
+  };
+  return (
+    <div
+      className={`z-50 fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div className="bg-white w-1/2 p-6 rounded shadow-lg absolute top-48 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <p className="text-l  mb-4">Update Task</p>
+        <div onClick={onClose} className="absolute right-8 top-6">
+          <IoMdClose />
+        </div>
+        <div className="flex justify-center">
+          <form
+            className="bg-white rounded mb-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col gap-4 justify-center">
+              <div>
+                <input
+                  className={`shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outlin`}
+                  id="name"
+                  type="text"
+                  placeholder="Task"
+                  {...register("name", { required: "Name is required" })}
+                />
+              </div>
+
+              <div>
+                <textarea
+                  className={`shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none `}
+                  id="description"
+                  placeholder="Description"
+                  {...register("description", {
+                    required: "Description is required"
+                  })}
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-red-200 text-black py-2 px-2 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Save
+                </button>
+              </div>
+              
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteModal;
